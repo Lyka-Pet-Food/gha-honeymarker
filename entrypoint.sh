3ac3ce25b4abbc896b5727bd83ca8f35cd2163f0
adampapadopoulos <adam.papadopoulos@lyka.com>
2024-02-02 08:46:52 +1100

    Double quoted create marker params

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ function install_binary() {
 }
 
 function create_marker() {
-    honeymarker -k $INPUT_APIKEY -d $INPUT_DATASET add -t $INPUT_TYPE -m $INPUT_MESSAGE
+    honeymarker -k "$INPUT_APIKEY" -d "$INPUT_DATASET" add -t "$INPUT_TYPE" -m "$INPUT_MESSAGE"
 }
 
 install_binary
